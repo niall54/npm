4a499b3b4d04ea8e17ffcaa164b1c72385726ba7
Niall Moroney <44774953+niall54@users.noreply.github.com>
2020-05-20 13:59:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='npm',
-  version='0.1.0',
+  version='0.1.1',
   author='Niall Moroney',
   author_email='niall_moroney@hotmail.co.uk',
   packages=['npm'],
